7ff02b721e95779c0be3a97fc7e8b097bc2561e7
Luke Brady <lbrad23105@gmail.com>
2018-04-24 13:14:01 -0400

    Started adding the functionality to translate facter output to JSON.

--- src/facter.go
+++ src/facter.go
@@ -1,6 +1,25 @@
 package main
 
-// RunFacter executes facter with the command given.
-func RunFacter(command string) {
+import (
+	"os/exec"
+)
 
+// RunFacter executes facter with the command given.
+func RunFacter(command string) (string, error) {
+	facter := exec.Command("facter", command)
+	err := facter.Run()
+	if err != nil {
+		return "", err
+	}
+	output, err := facter.Output()
+	if err != nil {
+		return "", err
+	}
+	return string(output), nil
+}
+
+// FacterToJSON converts facter's output to JSON and then
+// returns the JSON so that it can be served.
+func FacterToJSON(output string) []byte {
+	re := 
 }
--- src/route.go
+++ src/route.go
@@ -12,8 +12,7 @@ func IndexRoute(w http.ResponseWriter, r *http.Request) {
 	indexResponse := "HTTP-Facter\n"
 	indexResponse += "-----------\n"
 	indexResponse += "http-facter can be used to get facts from a remote server.\n\n"
-	indexResponse += "Use /os to gather facts about the OS.\n"
-	indexResponse += "Use /memory to gather facts about the systems memory.\n"
+	indexResponse += "For example, use /fact/os to gather facts about the OS.\n"
 	// Check to make sure the API is not being accesses by POST, PUT, etc.
 	if r.Method != "GET" {
 		w.WriteHeader(http.StatusForbidden)
@@ -32,7 +31,19 @@ func FactRoute(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusForbidden)
 		w.Write([]byte(forbidden))
 	} else {
-		TranslateRoute(r.RequestURI)
+		// Translate the route into a command.
+		command, err := TranslateRoute(r.RequestURI)
+		if err != nil {
+
+		}
+		// Run facter on the system and return the output.
+		output, err := RunFacter(command)
+		if err != nil {
+
+		}
+		factJSON := FacterToJSON(output)
+		w.WriteHeader(http.StatusOK)
+		w.Write(factJSON)
 	}
 }
 
